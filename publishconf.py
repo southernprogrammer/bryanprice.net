add9204cbb0bf38aef5fc2b6c34ce144e8506337
Bryan Price <southernprogrammer@gmail.com>
2025-08-18 07:51:28 -0500

    Went ahead and made SITEURL use https, because not setting a protocol doesn't work.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'bryanprice.net'
+SITEURL = 'https://bryanprice.net'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
